8c51e3e7d582d4a34b50568b1319a52ff7fe55c9
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-28 21:29:25 +0100

    feat: model `FeedItem`

---
+++ Yolo/Modules/Feed/Feature/FeedItem.swift
@@ -0,0 +1,47 @@
+//
+//  FeedItem.swift
+//  Yolo
+//
+//  Created by Gordon Smith on 28/04/2021.
+//
+
+import Foundation
+
+public struct FeedItem {
+    
+    public let id: String
+    public let imageURL: URL
+    public let user: User
+    public let interactions: Interactions
+    
+    public struct User {
+        public let id: String
+        public let name: String
+        public let about: String
+        public let imageURL: String
+        public init(id: String, name: String, about: String, imageURL: String) {
+            self.id = id
+            self.name = name
+            self.about = about
+            self.imageURL = imageURL
+        }
+    }
+    
+    public struct Interactions {
+        public let likes: Int
+        public let comments: Int
+        public let shares: Int
+        public init(likes: Int, comments: Int, shares: Int) {
+            self.likes = likes
+            self.comments = comments
+            self.shares = shares
+        }
+    }
+    
+    public init(id: String, imageURL: URL, user: User, interactions: Interactions) {
+        self.id = id
+        self.imageURL = imageURL
+        self.user = user
+        self.interactions = interactions
+    }
+}
