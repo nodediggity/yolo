42f34d7db5adde95a4715efcdb382d2eca90d695
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-28 20:15:29 +0100

    test: add common test helpers

---
+++ YoloTests/Modules/TestHelpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  YoloTests
+//
+//  Created by Gordon Smith on 28/04/2021.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
