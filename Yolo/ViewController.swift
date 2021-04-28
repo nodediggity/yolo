a5b5d144b34ac7fd566671612042d32324ff322d
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-28 19:17:23 +0100

    chore: create base project

---
+++ Yolo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yolo
+//
+//  Created by Gordon Smith on 28/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
