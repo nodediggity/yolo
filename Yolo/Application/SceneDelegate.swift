92be3cc654b884e10c6c1f470036e995770ecd23
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-28 19:59:43 +0100

    feat: set root view controller

--- Yolo/Application/SceneDelegate.swift
+++ Yolo/Application/SceneDelegate.swift
@@ -12,7 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        if let scene = (scene as? UIWindowScene) {
+            let window = UIWindow(windowScene: scene)
+            window.rootViewController = .init()
+            window.makeKeyAndVisible()
+            self.window = window
+        }
     }
 }
 
