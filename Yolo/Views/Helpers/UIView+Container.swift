b18883403eb1f744bf43d5f79ae0d1896ae59ca5
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-29 10:44:49 +0100

    refactor: move view helpers to shared scope

---
+++ Yolo/Views/Helpers/UIView+Container.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Container.swift
+//  Yolo
+//
+//  Created by Gordon Smith on 29/04/2021.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    func makeContainer() -> UIView {
+        let container = UIView()
+        container.backgroundColor = .clear
+        container.addSubview(self)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: trailingAnchor),
+            topAnchor.constraint(equalTo: container.topAnchor),
+            container.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        
+        return container
+    }
+    
+}
