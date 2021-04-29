895c27fee6ea3c573ee3b8b88ac2f8cf5ba20a00
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-29 05:58:13 +0100

    feat: create basic `FeedCardView`

---
+++ Yolo/Modules/Feed/UI/FeedCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCardView.swift
+//  Yolo
+//
+//  Created by Gordon Smith on 29/04/2021.
+//
+
+import UIKit
+
+public final class FeedCardView: UITableViewCell {
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    public required init?(coder: NSCoder) {
+        return nil
+    }
+}
