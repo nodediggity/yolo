fbde3d374f0998ace5a8b8c771127e133d13a88a
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-29 20:24:48 +0100

    refactor: use new `ResourcePresentationAdapter` for handling image state mapping

--- Yolo/Scenes/Feed/FeedUIComposer.swift
+++ Yolo/Scenes/Feed/FeedUIComposer.swift
@@ -42,8 +42,6 @@ private final class FeedViewAdapter {
     private let imageLoader: FeedUIComposer.ImageLoader
     private let selection: FeedUIComposer.SelectionHandler
     
-    private var cancellables: [URL: AnyCancellable] = [:]
-    
     init(controller: FeedViewController, imageLoader: @escaping FeedUIComposer.ImageLoader, selection: @escaping FeedUIComposer.SelectionHandler) {
         self.controller = controller
         self.imageLoader = imageLoader
@@ -58,42 +56,63 @@ extension FeedViewAdapter: ResourceView {
             let model = FeedCardPresenter.map(item)
             let view = FeedCardCellController(model: model)
             
+            // MARK:- UserImageView
+            let userImageViewAdapter = ResourcePresentationAdapter<Data, ResourceViewAdapter<UIImage>>(service: { [imageLoader] in
+                imageLoader(item.user.imageURL)
+            })
+            
+            userImageViewAdapter.presenter = ResourcePresenter(
+                view: ResourceViewAdapter { [weak view] in view?.displayImage(for: .user($0)) },
+                mapper: UIImage.tryMake(data:)
+            )
+                        
+            // MARK:- BodyImageView
+            let bodyImageViewAdapter = ResourcePresentationAdapter<Data, ResourceViewAdapter<UIImage>>(service: { [imageLoader] in
+                imageLoader(item.imageURL)
+            })
+            
+            bodyImageViewAdapter.presenter = ResourcePresenter(
+                view: ResourceViewAdapter { [weak view] in view?.displayImage(for: .body($0)) },
+                mapper: UIImage.tryMake(data:)
+            )
+            
+            view.onLoadImage = {
+                userImageViewAdapter.execute()
+                bodyImageViewAdapter.execute()
+            }
+            
+            view.onLoadImageCancel = {
+                userImageViewAdapter.cancel()
+                bodyImageViewAdapter.cancel()
+            }
+            
             view.onSelection = { [selection] in
                 selection(item)
-            }
-            
-            view.onLoadImage = { [loadImage, weak view] in
-                
-                loadImage(item.user.imageURL) {
-                    view?.displayImage(for: .user($0))
-                }
-                
-                loadImage(item.imageURL) {
-                    view?.displayImage(for: .body($0))
-                }
-            }
-            
-            view.onLoadImageCancel = { [weak self] in
-                [item.user.imageURL, item.imageURL].forEach { url in
-                    self?.cancellables[url]?.cancel()
-                    self?.cancellables[url] = nil
-                }
             }
             
             return view
         })
     }
-    
-    func loadImage(for url: URL, completion: @escaping (UIImage?) -> Void) {
-        guard cancellables[url] == nil else { return }
-        cancellables[url] = imageLoader(url)
-            .dispatchOnMainQueue()
-            .sink(
-                receiveCompletion: { _ in
-                },
-                receiveValue: { imageData in
-                    completion(UIImage.init(data: imageData))
-                }
-            )
+}
+
+public final class ResourceViewAdapter<ResourceViewModel>: ResourceView {
+    private var handler: () -> (ResourceViewModel) -> Void
+
+    public init(handler: @escaping (ResourceViewModel) -> Void) {
+        self.handler = { handler }
+    }
+
+    public func display(_ viewModel: ResourceViewModel) {
+        handler()(viewModel)
+    }
+}
+
+public extension UIImage {
+    struct InvalidImageData: Error {}
+    static func tryMake(data: Data) throws -> UIImage {
+        guard let image = UIImage(data: data) else {
+            throw InvalidImageData()
+        }
+        return image
     }
 }
