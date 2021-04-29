a103c54906a278f9103f9c27a7dbf8e110b999ad
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-29 18:02:00 +0100

    chore: remove unused methods in FeedPresenter

--- Yolo/Modules/Feed/Presentation/FeedPresenter.swift
+++ Yolo/Modules/Feed/Presentation/FeedPresenter.swift
@@ -7,41 +7,12 @@
 
 import Foundation
 
-public struct FeedLoadingViewModel {
-    public let isLoading: Bool
-}
-
-public protocol FeedLoadingView: class {
-    func display(_ viewModel: FeedLoadingViewModel)
-}
-
 public struct FeedViewModel {
     public let feed: [FeedItem]
 }
 
-public protocol FeedView: class {
-    func display(_ viewModel: FeedViewModel)
-}
-
-public final class FeedPresenter {
+public enum FeedPresenter {
     public static var title: String {
         "Discover"
-    }
-    
-    private var view: FeedView?
-    private weak var loadingView: FeedLoadingView?
-    
-    init(view: FeedView?, loadingView: FeedLoadingView? = nil) {
-        self.view = view
-        self.loadingView = loadingView
-    }
-    
-    public func didStartLoadingFeed() {
-        loadingView?.display(FeedLoadingViewModel(isLoading: true))
-    }
-    
-    public func didFinishLoadingFeed(with feed: [FeedItem]) {
-        view?.display(FeedViewModel(feed: feed))
-        loadingView?.display(FeedLoadingViewModel(isLoading: false))
     }
 }
