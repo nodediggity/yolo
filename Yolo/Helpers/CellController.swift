fd314443a01922a8e066d7287c13eeead2bf4ada
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-29 20:42:56 +0100

    feat: create cell controller container in preparation for adding diffable datasource support

---
+++ Yolo/Helpers/CellController.swift
@@ -0,0 +1,34 @@
+//
+//  CellController.swift
+//  Yolo
+//
+//  Created by Gordon Smith on 29/04/2021.
+//
+
+import UIKit
+
+public struct CellController {
+    let id: AnyHashable
+    let dataSource: UITableViewDataSource
+    let delegate: UITableViewDelegate?
+    let dataSourcePrefetching: UITableViewDataSourcePrefetching?
+
+    public init(id: AnyHashable = UUID(), _ dataSource: UITableViewDataSource) {
+        self.id = id
+        self.dataSource = dataSource
+        delegate = dataSource as? UITableViewDelegate
+        dataSourcePrefetching = dataSource as? UITableViewDataSourcePrefetching
+    }
+}
+
+extension CellController: Equatable {
+    public static func == (lhs: CellController, rhs: CellController) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+extension CellController: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
