08ba5c4c68b471b767a73d3a49576b723d27593c
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-30 08:52:50 +0100

    feat: implement `ContentResponseMapper`

---
+++ Yolo/Modules/Content/API/ContentResponseMapper.swift
@@ -0,0 +1,65 @@
+//
+//  ContentResponseMapper.swift
+//  Yolo
+//
+//  Created by Gordon Smith on 30/04/2021.
+//
+
+import Foundation
+
+public enum ContentResponseMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Content {
+        let decoder = JSONDecoder()
+        guard isOK(response), let root = try? decoder.decode(Root.self, from: data) else {
+            throw Error.invalidData
+        }
+        return root.asContentDTO
+    }
+}
+
+private extension ContentResponseMapper {
+    static var OK_200: Int { 200 }
+    
+    static func isOK(_ response: HTTPURLResponse) -> Bool {
+        response.statusCode == OK_200
+    }
+    
+    struct Root: Decodable {
+        let content: RemoteContent
+        
+        struct RemoteContent: Decodable {
+            let id: String
+            let imageURL: URL
+            let user: User
+            let interactions: Interactions
+            
+            struct User: Decodable {
+                let id: String
+            }
+            
+            struct Interactions: Decodable {
+                let likes: Int
+                let comments: Int
+                let shares: Int
+            }
+        }
+        
+        var asContentDTO: Content {
+            Content(
+                id: content.id,
+                imageURL: content.imageURL,
+                user: Content.User(id: content.user.id),
+                interactions: Content.Interactions(
+                    likes: content.interactions.likes,
+                    comments: content.interactions.comments,
+                    shares: content.interactions.shares
+                )
+            )
+        }
+    }
+}
+
---
+++ YoloTests/Modules/Content/API/ContentResponseMapperTests.swift
@@ -0,0 +1,90 @@
+//
+//  ContentResponseMapperTests.swift
+//  YoloTests
+//
+//  Created by Gordon Smith on 30/04/2021.
+//
+
+import XCTest
+import Yolo
+
+class ContentResponseMapperTests: XCTestCase {
+    
+    func test_map_throws_error_on_non_200_HTTPResponse() throws {
+        let data = makeJSONData(for: [:])
+        let samples = [199, 201, 300, 400, 500]
+        
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ContentResponseMapper.map(data, from: HTTPURLResponse(statusCode: code))
+            )
+        }
+    }
+    
+    func test_map_throws_error_on_200_HTTPResponse_with_invalid_json() {
+        let invalidJSONData = Data("any invalid data".utf8)
+        
+        XCTAssertThrowsError(
+            try ContentResponseMapper.map(invalidJSONData, from: HTTPURLResponse(statusCode: 200))
+        )
+    }
+    
+    func test_map_delivers_no_items_on_200_HTTPResponse_with_empty_json_list() throws {
+        let (model, json) = makeContent()
+        let data = makeJSONData(for: json)
+        
+        let output = try ContentResponseMapper.map(data, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(output, model)
+    }
+    
+    func test_map_delivers_items_on_200_HTTPResponse_with_json_items() throws {
+        let (model, json) = makeContent()
+        let data = makeJSONData(for: json)
+        
+        let output = try ContentResponseMapper.map(data, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(output, model)
+    }
+}
+
+private extension ContentResponseMapperTests {
+    
+    func makeJSONData(for content: [String: Any]) -> Data {
+        let data = try! JSONSerialization.data(withJSONObject: [
+            "content": content
+        ])
+        return data
+    }
+    
+    func makeContent() -> (model: Content, json: [String: Any]) {
+        let ITEM_ID = UUID().uuidString
+        let IMAGE_URL = "https://some-image.com"
+        let USER_ID = UUID().uuidString
+        let LIKES = 5
+        let COMMENTS = 10
+        let SHARES = 12
+        
+        let model = Content(
+            id: ITEM_ID,
+            imageURL: makeURL(IMAGE_URL),
+            user: Content.User(id: USER_ID),
+            interactions: Content.Interactions(likes: LIKES, comments: COMMENTS, shares: SHARES)
+        )
+        
+        let json = [
+            "id": ITEM_ID,
+            "imageURL": IMAGE_URL,
+            "user": [
+                "id": USER_ID
+            ],
+            "interactions": [
+                "likes": LIKES,
+                "comments": COMMENTS,
+                "shares": SHARES
+            ]
+        ] as [String : Any]
+        
+        return (model, json)
+    }
+}
