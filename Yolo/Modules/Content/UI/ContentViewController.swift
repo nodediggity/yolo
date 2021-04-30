abc8264701b652754e583921f85aa7b3a487dfbf
Gordon Smith <iamgordonsmith@icloud.com>
2021-04-30 10:31:10 +0100

    feat: content loads image for url when visible

--- Yolo/Modules/Content/UI/ContentViewController.swift
+++ Yolo/Modules/Content/UI/ContentViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 public final class ContentViewController: NSObject {
 
+    public var onLoadImage: (() -> Void)?
+    
     private var cell: ContentView?
     private var model: Content?
     
@@ -31,7 +33,15 @@ extension ContentViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         cell = tableView.dequeueReusableCell()
         configureUI()
+        load()
         return cell!
+    }
+}
+
+extension ContentViewController: ResourceView {
+    public typealias ResourceViewModel = UIImage
+    public func display(_ viewModel: ResourceViewModel) {
+        cell?.contentImageView.image = viewModel
     }
 }
 
@@ -42,5 +52,9 @@ private extension ContentViewController {
         cell?.likesCountLabel.text = "\(model.interactions.likes)"
         cell?.commentsCountLabel.text = "\(model.interactions.comments)"
         cell?.sharesCountLabel.text = "\(model.interactions.shares)"
+    }
+    
+    func load() {
+        onLoadImage?()
     }
 }
