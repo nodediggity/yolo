415b56b0d7c5343bedce19fe78fc9436640cf7a2
Gordon Smith <iamgordonsmith@icloud.com>
2021-07-26 19:09:05 +0100

    test: on init with no initial state delivers reducer default state

--- YoloTests/Helpers/StateContainerTests.swift
+++ YoloTests/Helpers/StateContainerTests.swift
@@ -8,11 +8,17 @@
 import XCTest
 import Combine
 
+typealias StateMapper<T> = (_ state: T?) -> T
+
 class StateContainer<T> {
     private(set) var state: CurrentValueSubject<T, Never>
 
-    init(state: T) {
-        self.state = .init(state)
+    init(state: T?, mapper: @escaping StateMapper<T>) {
+        if let state = state {
+            self.state = .init(state)
+        } else {
+            self.state = .init(mapper(nil))
+        }
     }
 }
 
@@ -20,7 +26,17 @@ class StateContainerTests: XCTestCase {
 
     func test_on_init_emits_initial_state() {
         let state = "initial state"
-        let sut = StateContainer<String>(state: state)
+        let sut = StateContainer<String>(state: state, mapper: { _ in state })
+        var output: [String] = []
+        _ = sut.state
+            .sink(receiveValue: { output.append($0) })
+        
+        XCTAssertEqual(output, [state])
+    }
+    
+    func test_on_init_with_no_initial_state_delivers_reducer_default_state() {
+        let state = "mapper state"
+        let sut = StateContainer<String>(state: nil, mapper: { _ in state })
         var output: [String] = []
         _ = sut.state
             .sink(receiveValue: { output.append($0) })
